0f580cdd5171e63b4ada3ed6c673c850df0537a9
AlShPhoenix <alok.11911001@iiitsonepat.ac.in>
2023-04-11 17:59:05 +0530

    updated blogs using props and useState

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
+import BlogList from "./BlogList";
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs,setBlogs]=useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
     return ( 
         <div className="home">
-            <h2>HomePage</h2>
+            <BlogList blogs={blogs} title="All Blogs!" /> 
         </div>
      );
 }

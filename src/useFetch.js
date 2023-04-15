95d0d25b21e7dbfd07095687031115b4e04f846b
AlShPhoenix <alok.11911001@iiitsonepat.ac.in>
2023-04-15 10:07:04 +0530

    added abortController and blog add functionality

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,6 +6,7 @@ const useFetch = (url) => {
     const [error,setError]=useState(null);
     
     useEffect( () =>{
+        const abortCont=new AbortController();
         setTimeout(() =>{
             fetch(url)
             .then(res =>{
@@ -20,10 +21,16 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err =>{
-                setIsPending(false);
-                setError(err.message);
-            })
+                if(err.name=="AbortError"){
+                    console.log('fetch aborted')
+        } else {
+          // auto catches network / connection error
+          setIsPending(false);
+          setError(err.message);
+        }
+      })
         },1000);
+        return ()=> abortCont.abort();
     },[url])
 
     return {data,isPending,error};
